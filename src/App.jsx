837fbe4cb955057d05e6fe7043c14990735cfce7
aHasnat1997 <hasnat56703@gmail.com>
2023-10-09 21:15:24 +0600

    Initial file add

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import logo from "./assets/logo.png";
+
+export default function App() {
+  return (
+    <main className="h-screen w-full flex items-center justify-center">
+      <img src={logo} alt="logo" />
+    </main>
+  )
+}
