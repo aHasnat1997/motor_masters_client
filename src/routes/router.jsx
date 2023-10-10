f4b9940f9ab69b445b09705f7ab331b22309e42c
aHasnat1997 <hasnat56703@gmail.com>
2023-10-10 19:43:33 +0600

    ADD: MainRouter, Lazy and CompLocations in routers file

--- src/routes/router.jsx
+++
@@ -1,9 +0,0 @@
-import { Route, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
-import App from "../App";
-
-
-export default mainRouter = createBrowserRouter(
-    createRoutesFromElements(
-        <Route path="/" element={<App />} />
-    )
-);
