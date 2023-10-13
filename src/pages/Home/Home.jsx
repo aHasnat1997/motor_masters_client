918b3f4fcbcb92a50125002ea3e18467d79bde01
aHasnat1997 <hasnat56703@gmail.com>
2023-10-13 23:37:44 +0600

    ADD: Home page and Banner for Home page

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import Banner from "./sections/Banner";
+
+/**
+ * Home page of the website.
+ * @returns {JSX.Element} Home
+ */
+export default function Home() {
+    return (
+        <div className="">
+            <Banner />
+        </div>
+    );
+}
